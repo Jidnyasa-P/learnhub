ced7cdf038fe3a05b1a5c8520a5e7a601ad4fbe3
udaycodespace <udaysomapuram@gmail.com>
2026-07-14 09:25:15 +0530

    fix: correct MONGO_URI env variable mismatch and update .env.example

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectionOfDb = () => {
   mongoose
-    .connect(process.env.MONGO_DB, {
+    .connect(process.env.MONGO_URI, {
       dbName: 'video-course-application',
       useNewUrlParser: true,
       useUnifiedTopology: true,
